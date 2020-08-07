2b304adc83912aabe23523198091543319bf089b
Недялко Ангелов <nedialkoangelov62@gmail.com>
2020-08-07 18:11:15 +0300

    Added eraser, brush

---
+++ app.js
@@ -0,0 +1,66 @@
+let canvas = $("#canvas");
+let colorPicker = $("#cp");
+let ctx = canvas[0].getContext("2d");
+let isDrawing = false;
+let btn = $(".btn");
+let dropdown = $("#dropdown-content");
+
+ctx.lineCap = 'round';
+ctx.beginPath();
+ctx.lineWidth = 1;
+
+canvas.mousedown(function (e) {
+
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = true;
+    Draw(x, y, e.offsetX, e.offsetY);
+});
+
+canvas.mousemove(function (e) {
+    if (isDrawing) {
+        Draw(x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+});
+
+canvas.mouseup(function (e) {
+    isDrawing = false;
+});
+
+canvas.mouseleave(function (e) {  // Won't draw when reentering the canvas if u hold click
+    isDrawing = false;
+});
+
+function Draw(x, y, x1, y1) {
+    ctx.moveTo(x, y);
+    ctx.lineTo(x1, y1);
+    ctx.stroke();
+}
+
+// COLOR CHANGER //
+colorPicker.change(function () {
+    ctx.beginPath();
+    let color = colorPicker.val();
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.closePath();
+})
+
+// ERASER //
+btn.click(function (e) {
+    ctx.beginPath();
+    ctx.strokeStyle = "#ffffff";
+    ctx.stroke();
+    ctx.closePath();
+})
+
+// DROPDOWN //
+dropdown.change(function () {
+    let val = this.value;
+
+    ctx.lineWidth = val; // 1,3,5,8
+    ctx.stroke();
+    ctx.closePath();
+})
