a912e43e24cc1b7e099970e27002cfeea5a03980
ned98 <37303080+ned98@users.noreply.github.com>
2020-08-09 15:12:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,61 +1,66 @@
 let canvas = $("#canvas");
-let colorPicker = $('#cp');
+let colorPicker = $("#cp");
 let ctx = canvas[0].getContext("2d");
-let x = 0;
-let y = 0;
 let isDrawing = false;
+let btn = $(".btn");
+let dropdown = $("#dropdown-content");
 
-ctx.beginPath();
-ctx.lineWidth = 5;
 ctx.lineCap = 'round';
-
-
-function color() {
-    ctx.beginPath();
-    var color = colorPicker.val();
-    ctx.strokeStyle = color;
-    ctx.stroke();
-    ctx.closePath();
-}
+ctx.beginPath();
+ctx.lineWidth = 1;
 
 canvas.mousedown(function (e) {
-    x = e.offsetX; // current mouse x coords
-    y = e.offsetY; // current mouse y coords
+
+    x = e.offsetX;
+    y = e.offsetY;
     isDrawing = true;
+    Draw(x, y, e.offsetX, e.offsetY);
 });
 
 canvas.mousemove(function (e) {
-
-    if (isDrawing) {                     // draw if it's clicked
+    if (isDrawing) {
         Draw(x, y, e.offsetX, e.offsetY);
         x = e.offsetX;
         y = e.offsetY;
     }
 });
 
-canvas.click(function (e) {
-
-    x = e.offsetX;
-    y = e.offsetY;
-    ctx.moveTo(x, y);
-    ctx.lineTo(x, y);
-    ctx.stroke();
-});
-
 canvas.mouseup(function (e) {
-
-    isDrawing = false;
-    x = 0;
-    y = 0;
-});
-
-canvas.mouseleave(function (e) {
     isDrawing = false;
 });
 
-function Draw(x1, y1, x2, y2) {
+canvas.mouseleave(function (e) {  // Won't draw when reentering the canvas if u hold click
+    isDrawing = false;
+});
 
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
+function Draw(x, y, x1, y1) {
+    ctx.moveTo(x, y);
+    ctx.lineTo(x1, y1);
     ctx.stroke();
-};
+}
+
+// COLOR CHANGER //
+colorPicker.change(function () {
+    ctx.beginPath();
+    let color = colorPicker.val();
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.closePath();
+})
+
+// ERASER //
+btn.click(function (e) {
+    ctx.beginPath();
+    ctx.strokeStyle = "#ffffff";
+    ctx.stroke();
+    ctx.closePath();
+})
+
+// DROPDOWN //
+dropdown.change(function () {
+    let val = this.value;
+
+    ctx.lineWidth = val; // 1,3,5,8
+    ctx.stroke();
+    ctx.closePath();
+})
